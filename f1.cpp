600e63a95c6d760795102ed8e949391c4fcb14ce
ayush <rakholiyaayush861@gmail.com>
2024-05-17 15:15:54 -0400

    made a struct called FlightInfo which has two pointers to a char for destination and date

--- f1.cpp
+++ f1.cpp
@@ -1,60 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <malloc.h>
-
-#pragma warning (disable: 4996)
-
-typedef struct {
-    char* destination;
-    char* date;
-} FlightInfo;
-
-void fillFlightInfo(FlightInfo* flightInfo, char* destination, char* date) {
-
-    
-
-    
-    flightInfo->destination = (char *) malloc((strlen(destination) + 1) * sizeof(char));// malloc (30 * sizeof(char)+1)
-    flightInfo->date = (char *) malloc((strlen(date) + 1) * sizeof(char));
-
-
-    strcpy(flightInfo->destination, destination);
-    strcpy(flightInfo->date, date);
-}
-
-void printFlightInfo(FlightInfo* flightInfo) {
-    for (int i = 0; i < 10; i++) {
-        printf("%-35s%-35s\n", flightInfo[i].destination, flightInfo[i].date);
-    }
-}
-
-int main() {
-    FlightInfo flightInfo[10];
-
-    for (int i = 0; i < 10; i++) {
-        char destination[30] = {0};
-        char date[30] = {0};
-
-        printf("Enter name of destination %d: ", i + 1);
-        
-        destination[strcspn(destination, "\n")] = '\0';
-        fgets(destination, sizeof(destination), stdin);
-
-        printf("Enter date of flight %d: ", i + 1);
-        
-        date[strcspn(date, "\n")] = '\0';
-        fgets(date, sizeof(date), stdin);
-
-        fillFlightInfo(&flightInfo[i], destination, date);
-    }
-
-
-    printFlightInfo(flightInfo);
-
-
-    free(flightInfo->destination);
-    free(flightInfo->date);
-
-    return 0;
-}
+/*
+* FILE : m4.cpp
+* PROJECT : SENG1000 - Major Assignment #4
+* PROGRAMMER : Ayushkumar Bhupatbhai Rakholiya
+* FIRST VERSION : 2024-04-1
+* DESCRIPTION :
+*   This program will takes in 10 pair of information about flights and stores the destination and 
+date in an array of structs and displayed it.
+*/
