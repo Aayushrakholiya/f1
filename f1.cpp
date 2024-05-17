a836ba8e02d40c2f0d18ad1fe4d075ad8ccf0915
ayush <rakholiyaayush861@gmail.com>
2024-05-17 15:28:23 -0400

    make one more function called printFlightInfo which has only one parameter which takes the array of struct and print all the information

--- f1.cpp
+++ f1.cpp
@@ -19,3 +19,17 @@ typedef struct {
     char* destination;
     char* date;
 } FlightInfo;
+
+void fillFlightInfo(FlightInfo* flightInfo, char* destination, char* date) {
+
+    flightInfo->destination = (char*)malloc((strlen(destination) + 1) * sizeof(char));
+    flightInfo->date = (char*)malloc((strlen(date) + 1) * sizeof(char));
+
+    strcpy(flightInfo->destination, destination);
+    strcpy(flightInfo->date, date);
+}
+void printFlightInfo(FlightInfo* flightInfo) {
+    for (int i = 0; i < 10; i++) {
+        printf("%-35s%-35s\n", flightInfo[i].destination, flightInfo[i].date);
+    }
+}
