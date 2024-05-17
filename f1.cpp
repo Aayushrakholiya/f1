0ac44f950b34d6aefa3bc6b229f176be19447e46
ayush <rakholiyaayush861@gmail.com>
2024-05-17 15:22:36 -0400

    made a function called fillFlightInfo which has 3 parameters one pointer to an element of the array and another two for flight information strings containing the destination and date

--- f1.cpp
+++ f1.cpp
@@ -7,3 +7,15 @@
 *   This program will takes in 10 pair of information about flights and stores the destination and 
 date in an array of structs and displayed it.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+
+#pragma warning (disable: 4996)
+
+typedef struct {
+    char* destination;
+    char* date;
+} FlightInfo;
